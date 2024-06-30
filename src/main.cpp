cdc1ae992fca22595d803967fcf8a6dfca54e63d
tranlin <tranlin_d@163.com>
2024-06-30 12:20:21 +0800

    update complie qrc files

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include <QApplication>
+#include <QHBoxLayout>
+#include <QIcon>
 #include <QLabel>
+#include <QMovie>
+#include <QPixmap>
+#include <QSlider>
+#include <QSpinBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <qfont.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-    QLabel label("Hello, world");
-    label.show();
+    QWidget      window;
+    QVBoxLayout  layout(&window);
+    QHBoxLayout  inner_layout;
+    QLabel       label_text("this is a demo");
+    QLabel       label_image;
+    QFont        ft;
+
+    ft.setPointSize(14);
+    label_text.setFont(ft);
+    inner_layout.addStretch();
+    inner_layout.addWidget(&label_text);
+    inner_layout.addStretch();
+    label_image.setPixmap(QPixmap(":/Image/demo"));
+    layout.addLayout(&inner_layout);
+    layout.addWidget(&label_image);
+    window.show();
     return app.exec();
 }
-
-
